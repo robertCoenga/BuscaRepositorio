4ae564e96408e2567d38e0985a847ef8d9aff217
Robert Coenga <robertcoenga@gmail.com>
2023-05-14 23:25:06 -0300

    Correção nos métodos Get

--- src/server.js
+++ src/server.js
@@ -1,17 +1,51 @@
 const express = require("express");
-
-//FAKE DATABASE:
-let repos = [];
-//services
+const fs = require('fs');
 const app = express();
-
+const repositorie = JSON.parse(fs.readFileSync('src/data/repositories_202305081745.json','utf-8'));
+const repos = repositorie.repositories;
+//services
 app.use(express.json());
 
-app.get('/repos/findByName', (req, res)=>{
-    return res.status(200).send("Olá Mundo");
-});
-app.get('repos/findById',(req,res)=>{
-    return res.status(200).send('Teste');
-});
 //Rodar o servidor
 app.listen(3333, ()=> console.log("Server is running"))
+
+app.get('/repos/findByName', (req, res)=>{
+    let queryRepoName = req.query.name;
+
+    if(queryRepoName==null)
+    {
+        return res.status(400).send({"mensagem":"O campo nome é obrigatório!"});
+    }
+    else{
+        let repo = repos.find(resp => resp.name === queryRepoName);       
+        if (repo!=null)
+        {
+            return res.status(200).send(repo);
+        }
+        else
+        {
+            return res.status(404).send({});
+        }
+    }
+})
+app.get('/repos/findById', (req,res)=>{
+    let queryRepoId = req.query.id;
+
+    if(queryRepoId==null)
+    {
+        return res.status(400).send({"mensagem":"O campo nome é obrigatório!"});
+    }
+    else{
+        let repo = repos.find(resp => resp.id === queryRepoId);       
+        if (repo!=null)
+        {
+            return res.status(200).send(repo);
+        }
+        else
+        {
+            return res.status(404).send({});
+        }
+    }
+});
+
+module.exports = app;
